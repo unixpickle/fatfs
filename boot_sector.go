7c8467ee5a75974bc5e82afb03f8e0fafb95ebab
Alex Nichol <unixpickle@gmail.com>
2018-11-24 14:53:39 -0500

    create a boot sector

--- boot_sector.go
+++ boot_sector.go
@@ -1,6 +1,53 @@
 package fatfs
 
+import (
+	"errors"
+	"math/rand"
+	"strings"
+)
+
+// BootSector contains the data in the first sector of the
+// volume.
 type BootSector [512]byte
+
+// NewBootSector32 creates a BootSector for a new FAT32
+// file-system.
+func NewBootSector32(volumeSize uint64, volumeLabel string) (*BootSector, error) {
+	if volumeSize < 512*65525 {
+		return nil, errors.New("volume is too small")
+	} else if volumeSize >= 512*(1<<32) {
+		return nil, errors.New("volume is too large")
+	}
+	res := new(BootSector)
+	copy(res.BootJump(), []byte{0xeb, 0, 0x90})
+	copy(res.OEMName(), []byte("MSWIN4.1"))
+	res.SetBytesPerSec(512)
+	res.SetSecPerClus(1)
+	res.SetRsvdSecCnt(2)
+	res.SetNumFATs(2)
+	res.SetRootEntCnt(0)
+	res.SetTotSec16(0)
+	res.SetMedia(0xf8)
+	res.SetFatSz16(0)
+	res.SetSecPerTrk(1)
+	res.SetNumHeads(1)
+	res.SetHiddSec(0)
+	res.SetTotSec32(uint32(volumeSize / 512))
+	res.SetFatSz32(0) // TODO: this.
+	res.SetExtFlags(0)
+	res.SetFSVer(0)
+	res.SetRootClus(2)
+	res.SetFSInfo(1)
+	res.SetBkBootSec(0)
+	res.SetDrvNum(0x80)
+	res.SetBootSig(0x29)
+	res.SetVolID(uint32(rand.Int31()))
+	copy(res.VolLab(), []byte(strings.ToUpper(volumeLabel)))
+	copy(res.FilSysType(), []byte("FAT32   "))
+	res[510] = 0x55
+	res[511] = 0xaa
+	return res, nil
+}
 
 func (b *BootSector) BootJump() []byte {
 	return b[0 : 0+3]
