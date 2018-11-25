dfe5f4cabfddda4968758fed2dcfcc7b042a48ce
Alex Nichol <unixpickle@gmail.com>
2018-11-25 09:19:49 -0500

    different sector & cluster sizes

--- dir.go
+++
@@ -1,6 +0,0 @@
-package fatfs
-
-// RootDirSector gets the sector of the root directory.
-func RootDirSector(b *BootSector) uint32 {
-	return uint32(b.RsvdSecCnt()) + uint32(b.NumFATs())*b.FatSz32() + b.RootClus()
-}
