65041ae3accf3a05f72209fcecfc64a4d7f4c25f
Alex Nichol <unixpickle@gmail.com>
2018-11-26 12:12:56 -0500

    proper FAT table entry updates

--- fs.go
+++ fs.go
@@ -1,6 +1,10 @@
 package fatfs
 
-import "github.com/unixpickle/essentials"
+import (
+	"errors"
+
+	"github.com/unixpickle/essentials"
+)
 
 // FS provides all the information needed to perform
 // file-system operations.
@@ -27,23 +31,39 @@ func NewFS(b BlockDevice) (*FS, error) {
 	return fs, nil
 }
 
-func (f *FS) readFAT(dataIndex uint32) (uint32, error) {
+// ClusterSize gets the number of bytes per cluster.
+func (f *FS) ClusterSize() int {
+	return int(f.BootSector.SecPerClus()) * SectorSize
+}
+
+// NumClusters gets the number of data clusters.
+func (f *FS) NumClusters() uint32 {
+	b := f.BootSector
+	numSectors := b.TotSec32() - (b.FatSz32()*uint32(b.NumFATs()) + uint32(b.RsvdSecCnt()))
+	return numSectors / uint32(b.SecPerClus())
+}
+
+// ReadFAT reads a FAT entry.
+func (f *FS) ReadFAT(dataIndex uint32) (uint32, error) {
 	sector, byteIdx := fatIndices(dataIndex)
 	block, err := f.Device.ReadSector(f.fatSectors[0] + sector)
 	if err != nil {
 		return 0, essentials.AddCtx("ReadFAT", err)
 	}
-	return Endian.Uint32(block[byteIdx : byteIdx+4]), nil
+	return Endian.Uint32(block[byteIdx:byteIdx+4]) & 0x0fffffff, nil
 }
 
-func (f *FS) writeFAT(dataIndex uint32, contents uint32) error {
+// WriteFAT writes a FAT entry.
+func (f *FS) WriteFAT(dataIndex uint32, contents uint32) error {
 	sector, byteIdx := fatIndices(dataIndex)
 	for _, sectorOffset := range f.fatSectors {
 		block, err := f.Device.ReadSector(sector + sectorOffset)
 		if err != nil {
 			return essentials.AddCtx("WriteFAT", err)
 		}
-		Endian.PutUint32(block[byteIdx:byteIdx+4], contents)
+		oldContents := Endian.Uint32(block[byteIdx : byteIdx+4])
+		newContents := (contents & 0x0fffffff) | (oldContents & 0xf0000000)
+		Endian.PutUint32(block[byteIdx:byteIdx+4], newContents)
 		err = f.Device.WriteSector(sector+sectorOffset, block)
 		if err != nil {
 			return essentials.AddCtx("WriteFAT", err)
